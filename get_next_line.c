2a099a6704a2265f09a8c3082b1ff4eba150f881
AquinoTH <thiago4912@hotmail.com>
2023-11-18 00:42:44 -0300

    updating repo - started gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,42 @@
 /*   By: taquino- <taquino-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 20:24:36 by taquino-          #+#    #+#             */
-/*   Updated: 2023/11/09 20:27:25 by taquino-         ###   ########.fr       */
+/*   Updated: 2023/11/17 23:34:55 by taquino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <fcntl.h>
+
 char	*get_next_line(int fd)
 {
+	
+	if (fd < 0)
+		return (NULL);
+	
+	
+
+
+
+
+
+		
 	return ;
 }
+
+int	main()
+{
+	int	fd;
+	char	*buff;
+
+	fd = open("file01.txt", O_RDONLY);
+	while (1)
+	{
+		buff = get_next_line(fd);
+		if (buff == NULL)
+			break;
+		printf("%s\n", buff);
+		free(buff);
+	}
+	return (0);
+}	
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,35 @@
 /*   By: taquino- <taquino-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 20:26:24 by taquino-          #+#    #+#             */
-/*   Updated: 2023/11/09 20:26:25 by taquino-         ###   ########.fr       */
+/*   Updated: 2023/11/18 00:30:36 by taquino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef get_next_line_h
+#define get_next_line_h
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 50
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct get_string
+{
+    char    c;
+    struct get_string *next;    
+} node;
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
