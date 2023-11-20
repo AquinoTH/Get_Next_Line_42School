03c616bdb5386f618b3440287788d9d236ee28bc
AquinoTH <thiago4912@hotmail.com>
2023-11-20 00:24:47 -0300

    gln in progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,157 @@
 /*   By: taquino- <taquino-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 20:24:36 by taquino-          #+#    #+#             */
-/*   Updated: 2023/11/17 23:34:55 by taquino-         ###   ########.fr       */
+/*   Updated: 2023/11/20 00:22:20 by taquino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
 
 char	*get_next_line(int fd)
 {
-	
-	if (fd < 0)
+	static s_list	*hidden;
+	char			*line;
+
+	hidden = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read (fd, &line, 0) < 0)
 		return (NULL);
-	
-	
-
-
-
-
-
-		
-	return ;
+	line = NULL;
+	// 1. read from fd and add to linked list;
+	read_and_hide(fd, &hidden);
+	if (hidden == NULL)
+		return NULL;
+	// 2. extract from hiding to line;
+	extract_line(hidden, &line);
+	// 3. clean up hidden;
+	clean_hidden(&hidden);	
+	if (line[0] == '\0')
+	{
+		free_hidden(hidden);
+		hidden = NULL;
+		free (line);
+		return (NULL);
+	}
+	return (line);
 }
+
+// Uses read() to add characters to the hiding;
+void	read_and_hide(int fd, s_list **hidden)
+{
+	char	*buff;
+	int	readed;
+	
+	readed = 1;
+	while (!found_newline(*hidden) && readed != 0)
+	{
+		buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (buff == NULL)
+			return;
+		readed = (int)read(fd, buff, BUFFER_SIZE);
+		if ((*hidden == NULL && readed == 0) || readed == -1)
+		{
+			free(buff);
+			return;
+		}		
+		buff[readed] = '\0';
+		add_to_hidden(hidden, buff, readed);
+		free(buff);
+	}
+}
+// Adds the content of our buffer to the end of our hiding.
+void	add_to_hidden(s_list **hidden, char *buff, int readed)
+{
+	int	i;
+	s_list	*last;
+	s_list	*new_node;
+
+	new_node = malloc(sizeof(s_list));
+	if (new_node == NULL)
+		return;
+	new_node->next = NULL;
+	new_node->content = malloc(sizeof(char) * (readed + 1));
+	if (new_node->content == NULL)
+		return;
+	i = 0;
+	while (buff[i] && i < readed)
+	{
+		new_node->content[i] = buff[i];
+		i++;
+	}
+	new_node->content[i] = '\0';
+	if (*hidden == NULL)
+	{
+		*hidden = new_node;
+		return;
+	}
+	last = ft_lst_get_last(*hidden);
+	last->next = new_node;
+}
+/* 
+	Extracts all characters from our hidden and stores then it out line
+	stopping after the first \n founded.
+*/
+void	extract_line(s_list *hidden, char **line)
+{
+	int	i;
+	int	j;
+
+	if (hidden == NULL)
+		return;
+	generate_line(line, hidden);
+	if (*line == NULL)
+		return;
+	j = 0;	
+	while (hidden)
+	{
+		i = 0;
+		while (hidden->content[i])
+		{
+			if (hidden->content[i] == '\n')
+			{
+				(*line)[j++] = hidden->content[i];
+				break;
+			}
+			(*line)[j++] = hidden->content[i++];
+		}
+		hidden = hidden->next;
+	}
+	(*line)[j] = '\0';
+}
+/*
+	After extracting the line that was read, we don't need those characters anymore.
+	This function clears the hiding so only the char that have not been returned at
+	the end of GNL() remain in our static hiding.
+*/
+void	clean_hidden(s_list **hidden)
+{
+	s_list	*last;
+	s_list	*clean_node;
+	int	i;
+	int	j;
+
+	clean_node = malloc(sizeof(s_list));
+	if (hidden == NULL || clean_node == NULL)
+		return;
+	clean_node->next = NULL;
+	last = ft_lst_get_last(*hidden);
+	i = 0;	
+	while (last->content[i] && last->content[i] != '\n')
+		i++;
+	if (last->content && last->content[i] == '\n')
+		i++;
+	clean_node->content = malloc(sizeof(char) * ((ft_strlen(last->content) - i) + 1));
+	if (clean_node->content == NULL)
+		return;
+	j = 0;
+	while (last->content[i])
+		clean_node->content[j++] = last->content[i++];
+	clean_node->content[j] = '\0';
+	free_hidden(*hidden);
+	*hidden = clean_node;
+}
+/*
+#include <stdio.h>
+#include <fcntl.h>
 
 int	main()
 {
@@ -38,10 +167,11 @@ int	main()
 	while (1)
 	{
 		buff = get_next_line(fd);
-		if (buff == NULL)
+		printf("%s", buff);
+		if (buff == NULL)	
 			break;
-		printf("%s\n", buff);
 		free(buff);
 	}
 	return (0);
 }	
+*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,97 @@
 /*   By: taquino- <taquino-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 20:25:56 by taquino-          #+#    #+#             */
-/*   Updated: 2023/11/17 22:48:19 by taquino-         ###   ########.fr       */
+/*   Updated: 2023/11/20 00:21:40 by taquino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    read_file()
+#include "get_next_line.h"
+
+
+// Looks for a newline char in the given linked list.
+
+int found_newline(s_list *hidden)
+{
+    int     i;
+    s_list *current;
+
+    if (hidden == NULL)
+        return (0);
+    current = ft_lst_get_last(hidden);
+    i = 0;
+    while (current->content[i])
+    {
+        if(current->content[i] == '\n')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+// Returns a pointer to the last node in our hidden.
+
+s_list  *ft_lst_get_last(s_list *hidden)
+{
+    s_list  *current;
+
+    current = hidden;
+    while (current && current->next)
+        current = current->next;
+    return (current);
+}
+
+/*
+    Calculates the number of chars in the current line
+    including the trailing \n if there's one, and allocates mem appropriately.
+*/
+
+void    generate_line(char **line, s_list *hidden)
+{
+    int i;
+    int len;
+
+    len = 0;
+    while (hidden)
+    {
+    	i = 0;
+        while (hidden->content[i])
+        {
+            if (hidden->content[i] == '\n')
+            {
+                len++;
+                break;
+            }
+            len++;
+            i++;
+        }
+        hidden = hidden->next;
+    }
+    *line = malloc(sizeof(char) * (len + 1));
+}
+
+// Frees the entire hidden
+
+void	free_hidden(s_list *hidden)
+{
+	s_list	*current;
+	s_list	*next;
+
+	current = hidden;
+	while (current)
+	{
+		free(current->content);
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
+
+int	ft_strlen(const char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*(str++))
+		len++;
+	return (len);
+}
